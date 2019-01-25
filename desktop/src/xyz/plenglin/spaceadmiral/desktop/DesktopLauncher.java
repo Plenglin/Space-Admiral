b502b23d1955aff175f23b3bfb830eb54ad79f23
Maxim <maximumpwnguin@gmail.com>
2019-01-25 14:07:58 -0800

    Initial Commit

---
+++ desktop/src/xyz/plenglin/spaceadmiral/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package xyz.plenglin.spaceadmiral.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import xyz.plenglin.spaceadmiral.SpaceAdmiral;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceAdmiral(), config);
+	}
+}
